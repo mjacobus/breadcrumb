7d13017d83053bb9659ee1052d4e0732e77eeb8c
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2013-12-30 14:48:59 -0200

    Installed simple coverage

--- breadcrumb.gemspec
+++ breadcrumb.gemspec
@@ -20,4 +20,6 @@ Gem::Specification.new do |s|
 
   s.add_development_dependency "rspec-rails"
   s.add_development_dependency "guard-rspec"
+  s.add_development_dependency "coveralls"
+  s.add_development_dependency "simplecov"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,14 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
+
+require 'simplecov'
+SimpleCov.start 'rails'
+
+if ENV['TRAVIS']
+  require 'coveralls'
+  Coveralls.wear!('rails')
+end
+
 require File.expand_path("../dummy/config/environment", __FILE__)
 
 require 'rspec/rails'
